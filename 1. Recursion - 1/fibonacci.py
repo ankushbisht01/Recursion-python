82a96f8bf5d23b91e73486d2feba5578dc1429b9
Ankush <ankushbisht72@gmail.com>
2021-09-09 20:18:10 +0530

    09 Sept 2021

---
+++ 1. Recursion - 1/fibonacci.py
@@ -0,0 +1,24 @@
+"""
+    Question -
+    Create a function that take a number and return the term correspomding to the number from fibonacci series
+
+    Sample Input -
+    5
+
+    Sample Output -
+    5
+
+"""
+
+def febon(n):
+    if n == 1 or n==2:
+        return 1
+    fabi_n_1 = febon(n-1)
+    fabi_n_2 = febon(n-2)
+    sum = fabi_n_1 + fabi_n_2
+    return sum
+
+if __name__=='__main__':
+    number = int(input('enter the number '))
+    term = febon(number)
+    print(term)
